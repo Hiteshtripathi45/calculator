1f4d642eb2008e7ae30838183cbe35961fa511dc
hitesh <hiteshtripathi91@gmail.com>
2024-08-07 15:38:53 +0530

    create logic number button

--- cal.js
+++ cal.js
@@ -1,11 +1,24 @@
 //for getting symbol of calculator//
 
 let symbol = document.getElementsByClassName('symbol')
-let sym;
+let sym ='';
 Array.from(symbol).forEach(element => {
     element.addEventListener('click',()=>{
-        sym=element.textContent
+        sym=Symbol(element.textContent)
 
     })
     
 });
+let num1 = ''
+let num2 = ''
+let num=document.getElementsByClassName('num')
+Array.from(num).forEach(nums =>{
+    nums.addEventListener('click',()=>{
+        if(sym=''){
+            num1=num1+nums.textContent
+        }
+        else{
+            num2=num2+nums.textContent
+        }
+    })
+})
